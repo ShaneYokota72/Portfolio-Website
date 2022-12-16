e6b1b03c1a80f79dfe6f6cc491b18d7ffedc2edf
ShaneYokota72 <shin20040720@gmail.com>
2022-12-15 23:54:28 -0800

    wasm edit

--- sum.cpp
+++ sum.cpp
@@ -1,18 +1,18 @@
 #include <iostream>
 #include <cstdlib>
-
+#include <emscripten/emscripten.h>
 using namespace std;
 
+
+EXTERN EMSCRIPTEN_KEEPALIVE int find_max_min(int num1, int num2, int num3){
+  if(num1<=num2 && num1<=num3){
+    return num1;
+  } else if(num2<=num1 && num2<=num3){
+    return num2;
+  } else {
+    return num3;
+  }
+}
 int main() {
-  // Get the two numbers entered by the user
-  int num1 = atoi(getenv("num1"));
-  int num2 = atoi(getenv("num2"));
-
-  // Calculate the sum of the two numbers
-  int sum = num1 + num2;
-
-  // Print the result to the screen
-  cout << "The sum of " << num1 << " and " << num2 << " is " << sum << endl;
-
   return 0;
 }
