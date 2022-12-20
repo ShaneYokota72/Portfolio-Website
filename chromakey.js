7cec9aca15812bbbcc6cc4d8ebc29c51b7f7bd28
ShaneYokota72 <shin20040720@gmail.com>
2022-12-19 20:34:00 -0800

    Script -> JS file

--- chromakey.js
+++ chromakey.js
@@ -1,6 +1,21 @@
 const input = document.getElementById('imageInput');
-const img = document.getElementById("my-image");
+    const img = document.getElementById("my-image");
+    input.addEventListener('change', function (){
+      const file = input.files[0];
+      const reader = new FileReader();
+      
+      reader.onload = function(){
+        img.src = reader.result;
+      }
 
-input.addEventListener('load', function (){
-    img.setAttribute('src', input.result);
-});
+      reader.readAsDataURL(file);
+    });
+    img.addEventListener('load', function(){
+      const canvas = document.getElementById('canvas');
+      const ctx = canvas.getContext("2d");
+      var width = img.clientWidth;
+      var height = img.clientHeight;
+      canvas.setAttribute('height', height);
+      canvas.setAttribute('width', width);
+      ctx.drawImage(img, 0, 0);
+    })
