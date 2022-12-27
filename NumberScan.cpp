33f6a3de65ca76d93783c4288caf3ea3a10c5ffc
ShaneYokota72 <shin20040720@gmail.com>
2022-12-26 21:39:50 -0800

    debugging sort algorithm

--- NumberScan.cpp
+++ NumberScan.cpp
@@ -660,71 +660,36 @@ void NumImg::sortDigitBlobs(){
 }
 
 int NumScan(int* lhsimg, int lhsw, int lhsh, int* rhsimg, int rhsw, int rhsh, int command){
-  int debug = command;
+    //int debug = command;
 
-  // ===================
-  // TO DO: Fill in the arguments to the constructors below
-  NumImg img1(lhsimg, lhsw, lhsh);
-  NumImg img2(rhsimg, rhsw, rhsh);
-  // ===================
-  // TO DO: call findAndCreateDigitBlobs on each img 
-  img1.findAndCreateDigitBlobs();
-  img2.findAndCreateDigitBlobs();
-  // ===================
-  // Complete - Do not alter
-  /* if(debug == 1) {
-    img1.printBoundingBoxes();
-  }
-  else if(debug == 2) {
-    img2.printBoundingBoxes();
-  }
-  else if(debug == 3) {
-    if(argc < 5){
-      cout << "Please provide an extra argument of the filename to save the image." << endl;
-    }
-    else {
-      img1.drawBoundingBoxesAndSave(argv[4]);
-    }
-  }
-  else if(debug == 4) {
-    if(argc < 5){
-      cout << "Please provide an extra argument of the filename to save the image." << endl;
-    }
-    else {
-      img2.drawBoundingBoxesAndSave(argv[4]);
-    }
-  }
-  else if(debug == 5 || debug == 7) {
-    // pass true if debug is 5 and false otherwise (i.e. 7)
-    string num = img1.classify( debug == 5 );
-    cout << "Image 1 digit string: " << num << endl;
-  }
-  else if(debug == 6 || debug == 8) {
-    // pass true if debug is 6 and false otherwise (i.e. 8)
-    string num = img2.classify( debug == 6 );
-    cout << "Image 2 digit string: " << num << endl;
-  }
-  else { */
-    // Complete - Do not alter - Default full implementation
+    // ===================
+    // TO DO: Fill in the arguments to the constructors below
+    NumImg img1(lhsimg, lhsw, lhsh);
+    NumImg img2(rhsimg, rhsw, rhsh);
+    // ===================
+    // TO DO: call findAndCreateDigitBlobs on each img 
+    img1.findAndCreateDigitBlobs();
+    img2.findAndCreateDigitBlobs();
+    // ===================
     string str1 = img1.classify(false);
     string str2 = img2.classify(false);
     cout << "str1: " << str1 << " str2: " << str2 << endl;
 
-    BigInt num1(str1);
+    /* BigInt num1(str1);
     BigInt num2(str2);
     BigInt sum = num1 + num2;
     cout << num1.to_string() << " + " << num2.to_string() 
-          << " = " << sum.to_string() << endl;
+            << " = " << sum.to_string() << endl;
     if(num1 < num2){
-      BigInt diff = num2 - num1;
-      cout << num2.to_string() << " - " << num1.to_string() 
+        BigInt diff = num2 - num1;
+        cout << num2.to_string() << " - " << num1.to_string() 
             << " = " << diff.to_string() << endl;
     }
     else {
-      BigInt diff = num1 - num2;
-      cout << num1.to_string() << " - " << num2.to_string() 
+        BigInt diff = num1 - num2;
+        cout << num1.to_string() << " - " << num2.to_string() 
             << " = " << diff.to_string() << endl;
-    }
+    } */
     /* } */ 
 
   return 0;
