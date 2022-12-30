106e4a629f225a5ba939c7e4f0e302d3b280b85c
ShaneYokota72 <shin20040720@gmail.com>
2022-12-29 21:02:00 -0800

    server directory write access denied

--- upload.php
+++ upload.php
@@ -6,16 +6,23 @@
       // Check if the uploaded files are BMP images
       if ($_FILES['image1']['type'] === 'image/bmp' && $_FILES['image2']['type'] === 'image/bmp') {
         // Set the target directory for the uploaded images
-        /* $target_dir = 'images/'; */
+        /* $target_dir = './'; */
         
         // Generate a unique filename for each image
         $image1_filename = uniqid().'.bmp';
         $image2_filename = uniqid().'.bmp';
         
         // Set the target path for each image
-        $image1_target_path = /* $target_dir. */$image1_filename;
-        $image2_target_path = /* $target_dir. */$image2_filename;
+        $image1_target_path = __DIR__.'/'.$image1_filename;
+        $image2_target_path = __DIR__.'/'.$image2_filename;
         
+        if (is_writable($image1_target_path) && is_writable($image2_target_path)) {
+          echo 'writeable';
+        } else {
+          echo 'errors witht he target path';
+        }
+
+
         // Try to move the uploaded images to the target directory
         if (move_uploaded_file($_FILES['image1']['tmp_name'], $image1_target_path) && move_uploaded_file($_FILES['image2']['tmp_name'], $image2_target_path)) {
           // The images were uploaded successfully
