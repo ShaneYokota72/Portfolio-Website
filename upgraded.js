b2715fc4651afe684c23f0cc9ecb5f7d81f2856c
Shane Yokota <shaneyok@usc.edu>
2023-01-25 15:13:23 -0800

    resume section added/calib sticky nav/ padding and margin edit

--- upgraded.js
+++ upgraded.js
@@ -2,7 +2,7 @@ var navbar = document.getElementsByClassName('navbarall');
 
 window.onscroll = function() {
   // pageYOffset or scrollY
-  if (window.pageYOffset > 350) {
+  if (window.pageYOffset > 310) {
     //alert("active");
     document.getElementsByClassName('navbarall')[0].style.backgroundColor = '#a2a2a2';
     // navbar.classList.add('scrolled')
