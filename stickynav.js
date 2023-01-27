08d9109cdc6fcd1eb3fb478ecff46d7c42bc0358
Shane Yokota <shaneyok@usc.edu>
2023-01-26 16:21:45 -0800

    media responsive edit

--- stickynav.js
+++ stickynav.js
@@ -2,7 +2,9 @@ var navbar = document.getElementsByClassName('navbarall');
 
 window.onscroll = function() {
   // pageYOffset or scrollY
-  if(window.innerWidth < 600 && window.pageYOffset > 150){
+  if(window.innerWidth < 400 && window.pageYOffset > 45){
+    document.getElementsByClassName('navbarall')[0].style.backgroundColor = '#a2a2a2';
+  } else if(window.innerWidth < 600 && window.pageYOffset > 150){
     document.getElementsByClassName('navbarall')[0].style.backgroundColor = '#a2a2a2';
   } else if(window.innerWidth < 800 && window.pageYOffset > 160){
     document.getElementsByClassName('navbarall')[0].style.backgroundColor = '#a2a2a2';
