07f7ca25b617780d2d19353c8e5d550972ce4b9d
Shane Yokota <shaneyok@usc.edu>
2023-02-26 22:23:55 -0800

    navbar colot edit/resume update

--- stickynav.js
+++ stickynav.js
@@ -3,15 +3,15 @@ var navbar = document.getElementsByClassName('navbarall');
 window.onscroll = function() {
   // pageYOffset or scrollY
   if(window.innerWidth < 400 && window.pageYOffset > 45){
-    document.getElementsByClassName('navbarall')[0].style.backgroundColor = '#a2a2a2';
+    document.getElementsByClassName('navbarall')[0].style.backgroundColor = '#222222';
   } else if(window.innerWidth < 600 && window.pageYOffset > 150){
-    document.getElementsByClassName('navbarall')[0].style.backgroundColor = '#a2a2a2';
+    document.getElementsByClassName('navbarall')[0].style.backgroundColor = '#222222';
   } else if(window.innerWidth < 800 && window.pageYOffset > 160){
-    document.getElementsByClassName('navbarall')[0].style.backgroundColor = '#a2a2a2';
+    document.getElementsByClassName('navbarall')[0].style.backgroundColor = '#222222';
   } else if(window.innerWidth < 1200 && window.pageYOffset > 160){
-    document.getElementsByClassName('navbarall')[0].style.backgroundColor = '#a2a2a2';
+    document.getElementsByClassName('navbarall')[0].style.backgroundColor = '#222222';
   } else if (window.pageYOffset > 310) {
-    document.getElementsByClassName('navbarall')[0].style.backgroundColor = '#a2a2a2';
+    document.getElementsByClassName('navbarall')[0].style.backgroundColor = '#222222';
   } else {
     document.getElementsByClassName('navbarall')[0].style.backgroundColor = 'transparent';
   }
